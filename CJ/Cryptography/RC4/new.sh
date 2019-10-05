5cd222bef321288accc4d7c5293726df0c4b847a
Mr. Do <bemr.do@gmail.com>
2019-10-05 17:32:52 +0800

    Cyber Jawara

---
+++ CJ/Cryptography/RC4/new.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+KEY=`hexdump -n 16 -e '4/4 "%08X" 1 "\n"' /dev/random`
+#cat "CYBER JAWARA 2019 QUALS - RULES-OF-THE-GAME.pdf" | openssl rc4-40 -K $KEY -nosalt -e -nopad > "CYBER JAWARA 2019 QUALS - RULES-OF-THE-GAME.pdf.encrypted"
+#cat "flag.pdf" | openssl rc4-40 -K $KEY -nosalt -e -nopad > "flag.pdf.encrypted"
+cat "CYBER-JAWARA-2019-QUALS-RULES-OF-THE-GAME-1.pdf" | openssl rc4-40 -K $KEY -nosalt -e -nopad > "CJ1.pdf.enc"
+echo $KEY;
